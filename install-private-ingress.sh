05523c28b0ee5eb3774da37a13cf208a7718a56b
Trevor Linton <trevor.linton@gmail.com>
2019-08-20 11:04:09 -0600

    fixed private ingress

--- install-private-ingress.sh
+++ install-private-ingress.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-helm install --name nginx-ingress-i --namespace nginx-ingress-i stable/nginx-ingress \
-	--set controller.stats.enabled=true \
-	--set controller.metrics.enabled=true \
-	--set controller.ingressClass=nginx-internal \
-	--set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-type"="nlb" \
-	--set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-cross-zone-load-balancing-enabled"=true \
-	--set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-internal"="0.0.0.0/0" \
-	--wait --timeout 800
+echo "Installing Private Ingress... "
+
+if [ "$PRIVATE_INGRESS_USE_AWS_NLB" == "true" ]; then 
+	helm install --name nginx-ingress-i --namespace nginx-ingress-i stable/nginx-ingress \
+		--set controller.stats.enabled=true \
+		--set controller.metrics.enabled=true \
+		--set controller.ingressClass=nginx-internal \
+		--set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-type"="nlb" \
+		--set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-cross-zone-load-balancing-enabled"=true \
+		--set controller.service.annotations."service\.beta\.kubernetes\.io/aws-load-balancer-internal"="0.0.0.0/0" \
+		--wait --timeout 800
+else
+	helm install --name nginx-ingress-i --namespace nginx-ingress-i stable/nginx-ingress \
+		--set controller.stats.enabled=true \
+		--set controller.metrics.enabled=true \
+		--set controller.ingressClass=nginx-internal \
+		--wait --timeout 800
+if
+
+echo "Installing Private Ingress... Done"
